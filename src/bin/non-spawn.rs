8e864f061dae1c806d9a2a73692e7d782f7eb2df
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-11-01 07:12:40 +0800

    fix(non-spawn): add type Enter to exit

--- src/bin/non-spawn.rs
+++ src/bin/non-spawn.rs
@@ -11,6 +11,13 @@ fn main() {
         // Wait for the process to complete
         let _ = child.wait().expect("Failed to wait for game process");
         println!("beatoraja process completed");
+
+        // Add any additional logic here
+        println!("Type Enter to exit...");
+        let mut buf = String::new();
+        std::io::stdin()
+            .read_line(&mut buf)
+            .expect("Failed to read input");
     }
 
     #[cfg(not(target_os = "windows"))]
