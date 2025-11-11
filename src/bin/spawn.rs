d5aca453d357567ff3add83d6b53cde954a3ebda
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-11-12 05:58:50 +0800

    fix: clippy

--- src/bin/spawn.rs
+++ src/bin/spawn.rs
@@ -6,6 +6,7 @@ fn main() {
         let mut command = create_beatoraja_command_with_javaw();
 
         println!("Starting beatoraja with spawn...");
+        #[allow(clippy::zombie_processes)]
         let _ = command.spawn().expect("Failed to run game");
         println!("Started beatoraja with spawn...");
     }
