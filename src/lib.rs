9049f0362f936d4995f9d7d92059cbc20115b1b6
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-25 11:37:10 +0800

    feat: add cross-platform support for non-Windows systems

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::process::{Command, Stdio};
 
-#[cfg(target_os = "windows")]
 fn add_common_args(mut command: Command) -> Command {
     command
         .arg("-Xms1g")
@@ -14,13 +13,18 @@ fn add_common_args(mut command: Command) -> Command {
         .arg("-XX:+UseThreadPriorities")
         .arg("-XX:+ShowCodeDetailsInExceptionMessages")
         .arg("-cp")
-        .arg(r"beatoraja.jar;ir/*")
+        .arg(CLASSPATH)
         .arg("bms.player.beatoraja.MainLoader")
         .args(std::env::args().skip(1));
     command
 }
 
 #[cfg(target_os = "windows")]
+const CLASSPATH: &str = r"beatoraja.jar;ir/*";
+
+#[cfg(not(target_os = "windows"))]
+const CLASSPATH: &str = "beatoraja.jar:ir/*";
+
 fn can_run(exe: &str) -> bool {
     Command::new(exe)
         .arg("-version")
@@ -30,7 +34,6 @@ fn can_run(exe: &str) -> bool {
         .is_ok()
 }
 
-#[cfg(target_os = "windows")]
 fn resolve_executable<'a>(candidates: &'a [&'a str]) -> &'a str {
     candidates
         .iter()
@@ -46,9 +49,23 @@ pub fn create_beatoraja_command() -> Command {
     add_common_args(command)
 }
 
+#[cfg(not(target_os = "windows"))]
+pub fn create_beatoraja_command() -> Command {
+    let exe = resolve_executable(&["./jre/bin/java", "./jdk/bin/java", "java"]);
+    let command = Command::new(exe);
+    add_common_args(command)
+}
+
 #[cfg(target_os = "windows")]
 pub fn create_beatoraja_command_with_javaw() -> Command {
     let exe = resolve_executable(&[r".\jre\bin\javaw.exe", r".\jdk\bin\javaw.exe", r"javaw.exe"]);
+    let command = Command::new(exe);
+    add_common_args(command)
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn create_beatoraja_command_with_javaw() -> Command {
+    let exe = resolve_executable(&["./jre/bin/javaw", "./jdk/bin/javaw", "javaw"]);
     let command = Command::new(exe);
     add_common_args(command)
 }
